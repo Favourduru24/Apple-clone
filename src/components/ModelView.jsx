8f05a718c0faff47f2046a3bc8d7f7a273ab1c46
unknown <Favourduru24@gmail.com>
2025-03-16 19:15:37 -0700

    working on stripe

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -46,7 +46,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
-import Phone from './IPhone';
+import Phone from './Phone';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
